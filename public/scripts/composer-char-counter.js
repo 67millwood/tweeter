5ff243e9c56502d02ad02c32f701e4ab29d6b831
67millwood <info@track2development.com>
2018-11-06 21:02:32 +0000

    finished red

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -4,9 +4,12 @@ $(document).ready(function() {
   console.log("the page has loaded")
 
   $(".container textarea").keyup( function() {
-  console.log(140 - $(this).val().length);
-  $(this).parent().find(".counter").text((140 - $(this).val().length));
-
+  let count = $(this).val().length;
+  if (140 < count) {
+    $(this).parent().find(".counter").text(140 - count).css({"color": "red"});
+  } else {
+    $(this).parent().find(".counter").text(140 - count);
+  }
 
 });
 });
