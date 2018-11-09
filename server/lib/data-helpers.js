f5f9bc931224ca8d9f2eb3ca482dcd423f433fdb
67millwood <info@track2development.com>
2018-11-09 16:52:37 +0000

    what

--- server/lib/data-helpers.js
+++ server/lib/data-helpers.js
@@ -9,21 +9,12 @@ module.exports = function makeDataHelpers(db) {
 
     // Saves a tweet to `db`
     saveTweet: function(newTweet, callback) {
-      simulateDelay(() => {
+
         // db.tweets.push(newTweet);
         db.collection("tweeter").insertOne( newTweet );
         callback(null, true);
-      });
     },
 
-    // Get all tweets in `db`, sorted by newest first
-    // getTweets: function(callback) {
-    //   db.collection("tweeter").find().toArray(callback);
-    //   simulateDelay(() => {
-    //     const sortNewestFirst = (a, b) => a.created_at - b.created_at;
-    //     callback(null, db.tweets);
-    //   });
-    // }
       getTweets: function getTweets(callback) {
         db.collection("tweeter").find().toArray((err, tweets) => {
           if (err) {
