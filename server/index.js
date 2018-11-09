9f497a464f6a4bd7801955cced9b163a39a52639
67millwood <info@track2development.com>
2018-11-09 05:36:29 +0000

    minor helper changes

--- server/index.js
+++ server/index.js
@@ -7,16 +7,28 @@ const express       = require("express");
 const bodyParser    = require("body-parser");
 const app           = express();
 
-
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
+
+
+const MongoClient = require("mongodb").MongoClient;
+const MONGODB_URI = "mongodb://localhost:27017/tweeter";
+
+MongoClient.connect(MONGODB_URI, (err, db) => {
+  if (err) {
+    console.error(`Failed to connect: ${MONGODB_URI}`);
+    throw err;
+  }
+  console.log(`Connected to mongodb way to go Keith: ${MONGODB_URI}`);
+
+
 // app.use(function (req, res, next) {
 //   console.log(req.body);
 //   next();
 // })
-app.use(express.static("public"));
 
-// The in-memory database of tweets. It's a basic object with an array in it.
-const db = require("./lib/in-memory-db");
+// The in-memory database of tweets. Ilst's a basic object with an array in it.
+// const db = require("./lib/in-memory-db");
 
 // The `data-helpers` module provides an interface to the database of tweets.
 // This simple interface layer has a big benefit: we could switch out the
@@ -33,6 +45,8 @@ const tweetsRoutes = require("./routes/tweets")(DataHelpers);
 
 // Mount the tweets routes at the "/tweets" path prefix:
 app.use("/tweets", tweetsRoutes);
+
+});
 
 app.listen(PORT, () => {
   console.log("Example app listening on port " + PORT);
